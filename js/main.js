494822ecfdab808bce46d5fda06e9f4d21f6f256
Biswajyoti Nath <biswajyotinath125@gmail.com>
2025-07-11 15:49:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Quantum Neon Portfolio - Enhanced JS
 
 // Typing Effect 
-const typingText = "💻 Computer Science Undergraduate | 🔏 Aspiring Researcher in Quantum Security | 🧑‍💻 Locked-In Coder"; 
+const typingText = "💻 Computer Science Undergraduate.\n🔏 Aspiring Researcher in Quantum Security\n🧑‍💻 Locked-In Coder"; 
 const typingElement = document.getElementById("typing"); 
 let index = 0;
 
